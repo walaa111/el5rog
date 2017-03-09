7cf6157ed5f2cc069f3140ff670651f4632f8a4e
walaa111 <walaagamil18@gmail.com>
2017-03-09 13:24:10 +0200

    Update my-app.js

--- el5rog/www/js/my-app.js
+++ el5rog/www/js/my-app.js
@@ -113,6 +113,33 @@ document.addEventListener('deviceready', initApp, false);
     //     alert(ser_id);
     //     window.location.href("ceaser.html");
     // });
+     function checkConnection() {
+           var networkState = navigator.network.connection.type;
+           var states = {};
+           states[Connection.UNKNOWN]  = 'Unknown connection';
+           states[Connection.ETHERNET] = 'Ethernet connection';
+           states[Connection.WIFI]     = 'WiFi connection';
+           states[Connection.CELL_2G]  = 'Cell 2G connection';
+           states[Connection.CELL_3G]  = 'Cell 3G connection';
+           states[Connection.CELL_4G]  = 'Cell 4G connection';
+           states[Connection.NONE]     = 'No network connection';
+          
+           return networkState;
+          
+       }
+
+
+       function onDeviceReady() {
+       var networkState = checkConnection();
+    /* load local files if there is not network connection */
+                  if (networkState == Connection.NONE) {
+                      window.location="local/index.html"; 
+                  } else {
+               window.location="https://www.ovplatform.tk/el5rog/www/";
+                  }
+     }
+
+
 document.addEventListener("deviceready", onDeviceReady, false); 
 function onDeviceReady() {
     // Now safe to use the Codova API
