ce31fb77961a8a8c8217eb68bb1c069511317269
walaa111 <walaagamil18@gmail.com>
2017-03-22 15:53:56 +0200

    Update index.js

--- el5rog/www/js/index.js
+++ el5rog/www/js/index.js
@@ -20,7 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-        document.addEventListener("backbutton", this.yourCallbackFunction, false);
+        document.addEventListener("backbutton", yourCallbackFunction, false);
 
     },
 
