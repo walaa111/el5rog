28778b0d5cd02963ba53beec41a4e32b35b15ae4
walaa111 <walaagamil18@gmail.com>
2017-03-23 15:17:37 +0200

    Update index.js

--- el5rog/www/js/index.js
+++ el5rog/www/js/index.js
@@ -23,7 +23,7 @@ var app = {
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
         document.addEventListener("backbutton", yourCallbackFunction, false);
-var networkState = navigator.connection.type;
+
 
 
     },
@@ -46,6 +46,21 @@ var networkState = navigator.connection.type;
 	*/
 	
 	//alert('end of ready');
+	     function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
 checkConnection();
 
 
@@ -69,19 +84,5 @@ checkConnection();
  navigator.app.exitApp(); 
  alert('exitapp');
     }
- function checkConnection() {
-    var networkState = navigator.connection.type;
 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
-
-    alert('Connection type: ' + states[networkState]);
-}
 app.initialize();
