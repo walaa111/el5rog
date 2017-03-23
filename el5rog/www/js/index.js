c658357823fe4f1625ee09b5f55ee6e4001f62b5
walaa111 <walaagamil18@gmail.com>
2017-03-23 16:01:25 +0200

    Update index.js

--- el5rog/www/js/index.js
+++ el5rog/www/js/index.js
@@ -46,8 +46,23 @@ var app = {
 	*/
 	
 	//alert('end of ready');
-
-
+function checkConnection() {
+    var networkState = navigator.connection.type;
+ 
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+ 
+    alert('Connection type: ' + states[networkState]);
+}
+ 
+checkConnection();
 
     },
 
@@ -64,10 +79,10 @@ var app = {
     }
 };
 
-  function yourCallbackFunction(e) {
+ /* function yourCallbackFunction(e) {
     e.preventDefault();
  navigator.app.exitApp(); 
- alert('exitapp');
+ //alert('exitapp');
     }
-
+*/
 app.initialize();
